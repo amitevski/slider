9612f9ab14f5d253bc95b8dde2345684be27f6e8
Aco Mitevski <aco.mitevski@gmail.com>
2016-08-11 11:01:03 +0200

    implement pagination

--- src/demo.ts
+++ src/demo.ts
@@ -1,6 +1,12 @@
 import {Slider} from './slider';
 
-let images = ['http://lorempixel.com/600/350/?1', 'http://lorempixel.com/600/350/?2' ];
+let images = [
+  'http://lorempixel.com/600/350/?1',
+  'http://lorempixel.com/600/350/?2',
+  'http://lorempixel.com/600/350/?3',
+  'http://lorempixel.com/600/350/?4',
+   'http://lorempixel.com/600/350/?5'
+];
 let elements = document.getElementsByClassName('slider');
 
 new Slider(elements[0], images);
--- src/slider.ts
+++ src/slider.ts
@@ -8,11 +8,14 @@ class Slider {
   currentSlide: number = 0;
   totalSlides: number = 0;
   imageContainer: Element;
+  imageElements: any[] = [];
+  counterElement: Element;
 
   constructor(private element: Element, private images: string[]) {
     this.totalSlides = this.images.length;
     this.addImageContainer();
-    this.images.forEach( this.addImage );
+    this.images.map( this.addImage.bind(this) );
+    this.imageElements[this.currentSlide].style.display = 'block';
     this.addCounter();
     this.addArrows();
   }
@@ -21,12 +24,16 @@ class Slider {
    * go to next image
    */
   next() {
+    console.log('clicked next');
+    this.gotoImage(this.currentSlide+1);
   }
 
   /**
    * go to previous image
    */
   prev() {
+    console.log('clicked prev');
+    this.gotoImage(this.currentSlide-1);
   }
 
   /**
@@ -37,13 +44,24 @@ class Slider {
      * if given number doesnt exist goto to end/beginning of images
      * update counter
      */
+    this.imageElements[this.currentSlide].style.display = 'none';
+    if (n > this.imageElements.length-1) {
+      this.currentSlide = 0;
+    } else if (n < 0) {
+      this.currentSlide = this.imageElements.length - 1;
+    } else {
+      this.currentSlide = n;
+    }
+
+    this.imageElements[this.currentSlide].style.display = 'block';
+    this.updateCounter();
   }
 
   /**
    * update counter element to display currently displayed image number
    */
   private updateCounter() {
-
+    this.counterElement.innerHTML = `${this.currentSlide+1} / ${this.totalSlides}`;
   }
 
 
@@ -60,14 +78,24 @@ class Slider {
    * add an image to the element
    */
   private addImage(imageSrc: string) {
-
+    let img = new Image();
+    img.src = imageSrc;
+    let imageSlide = document.createElement('div');
+    imageSlide.className = 'slider-img';
+    imageSlide.appendChild(img);
+    imageSlide.style.display = 'none';
+    this.imageElements.push(imageSlide);
+    this.imageContainer.appendChild(imageSlide);
   }
 
   /**
    * add a counter to the element
    */
   private addCounter() {
-
+    this.counterElement = document.createElement('div');
+    this.counterElement.className = 'slider-counter';
+    this.updateCounter();
+    this.element.appendChild(this.counterElement);
   }
 
   /**
@@ -76,6 +104,16 @@ class Slider {
   private addArrows() {
     let prevImg = '❮',
       nextImg = '❯';
+    let nextButton = document.createElement('button');
+    nextButton.className = 'slider-next';
+    nextButton.innerText = nextImg;
+    nextButton.onclick = this.next.bind(this);
+    let prevButton = document.createElement('button');
+    prevButton.className = 'slider-prev';
+    prevButton.innerText = prevImg;
+    prevButton.onclick = this.prev.bind(this);
+    this.element.appendChild(prevButton);
+    this.element.appendChild(nextButton);
   }
 
   /**
